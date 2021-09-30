47bd2262a7c52cb12640d44d89696607b9cfc3c5
amedeo <amedeopasanisi97@gmail.com>
2021-09-30 13:20:52 +0200

    creazione Header

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <img src="../assets/img/logo-sidearea-1.png" alt="">
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped lang="scss">
+footer {
+    height: 200px;
+    background-color: #d6e9de;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <header>
+        <img src="../assets/img/logo-sidearea-1.png" alt="">
+        <ul>
+            <li class="selected">HOME</li>
+            <li>PAGES</li>
+            <li>PORTFOLIOBLOG</li>
+            <li>SHOP</li>
+            <li>ELEMENTS</li>
+            <li><img src="../assets/svg/svg-1.svg" alt=""></li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 30px;
+    ul {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        list-style: none;
+        width: 40%;
+        li {
+            position: relative;
+            font-weight: 600;
+            font-size: 11px;
+        }
+        .selected::after {
+            position: absolute;
+            bottom: 2px;
+            left: 0;
+            content: "";
+            width: 100%;
+            border-bottom: 5px solid pink;
+            z-index: -1;
+        }
+    }
+}
+</style>
